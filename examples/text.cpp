8d19d3baff6115b8da683feb2d5efcbe511b0657
chad <chad@816618ff-971e-4f3c-9917-b646dc1eeecf>
2015-08-30 05:35:24 +0000

    Js for jacks and an examples directory.
    git-svn-id: http://www.cdkersey.com/log/2015.08.21/eeprom_prog@1701 816618ff-971e-4f3c-9917-b646dc1eeecf

---
+++ examples/text.cpp
@@ -0,0 +1,31 @@
+// Using the current set of open-source PCB layout tools feels like using
+// MS-paint for Windows 3.1 with hams duct-taped to my fingers. This is an
+// attempt to make something a little less bad. If you get it, you get it. If
+// not, please tell me what I'm doing wrong.
+
+#include <libpcb/text.h>
+
+using namespace std;
+using namespace libpcb;
+
+int main() {
+  using namespace libpcb;
+
+  new text(get_default_font(), LAYER_SILKSCREEN, point(-2, 0),
+	   "The quick brown fox jumps over the lazy dog.",
+	   1.0/32);
+
+  new text(get_default_font(), LAYER_SILKSCREEN, point(-2, -0.5),
+          "1-244-867-5309",
+	   1.0/32);
+
+  new text(get_default_font(), LAYER_SILKSCREEN, point(-2, -1),
+           "ABCDEFGHIJKLMNOPQRSTUVWXYZzyxwvutsrqponmlkjihgfedcba",
+           1.0/32);
+
+  ofstream gfile("dump.ss.grb");
+  gerber g(gfile);
+  drawable::draw_layer(LAYER_SILKSCREEN, g);
+
+  return 0;
+}
